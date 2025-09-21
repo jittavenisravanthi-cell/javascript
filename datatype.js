e66aa0e0e87d7e0a706fd30f45a8a6cfcee94989
sravanthi <jittavenisravanthi@gmail.c0m>
2025-09-22 00:38:05 +0530

    function

--- datatype.js
+++ datatype.js
@@ -1,6 +1,6 @@
-a=128;
-console.log(typeof (a));
-name="sravs";
-console.log(typeof (name));
-c=true;
-console.log(typeof (c));
+a = 128;
+console.log(typeof a);
+name = "sravs";
+console.log(typeof name);
+c = true;
+console.log(typeof c);
---
+++ function.js
@@ -0,0 +1,24 @@
+
+
+// const add =() => {
+//     var a=76;
+//     var b=65;
+//     document.write(a+b);
+//     console.log(a+b);
+// }
+// add()
+// //ES7
+//     const add = (a+b) => console.log(a + b);
+//     add(78,90);
+// //hosting - to declare the various to the top
+// console.log(b);
+// b = 45;
+
+let name = "john";
+
+function demo() {
+let name = "sravs";
+document.write('hello, my name is ${name}');
+}
+document.write('hello, my name is ${name}');
+demo();

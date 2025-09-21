708470e722de9f47b9dba148e5d82bd80776e90d
sravanthi <jittavenisravanthi@gmail.c0m>
2025-09-21 23:05:36 +0530

    variable

---
+++ app.js
@@ -0,0 +1,15 @@
+//types of variables
+//var,Let,const
+//var-global variable
+//Let,constant - Local variable
+
+var a = 67;
+var a = 90;
+let b = 68;
+
+
+const c = 45;
+
+console.log("value b is $(b)");
+
+console.log(a)

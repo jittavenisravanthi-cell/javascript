f58fc77e7aacd575493f56bea72c89130f5e0173
sravanthi <jittavenisravanthi@gmail.c0m>
2025-09-21 23:06:42 +0530

    datatype

--- app.js
+++ app.js
@@ -11,5 +11,11 @@ let b = 68;
 const c = 45;
 
 console.log("value b is $(b)");
-
+console.log("value c is $(c)");
 console.log(a)
+//comment 
+
+//commit used to understand the code of other developer
+//data types
+// 1.primitive 2. non-primitive
+//primitive - number,String Boolean,Null,undefined
